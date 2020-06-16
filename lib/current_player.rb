eb16aacd4908a23eae257c74a12138b085e2a02d
james wu <jwu_nyc@yahoo.com>
2020-06-16 01:28:01 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,27 @@
 
 def turn_count(board)
   count=0
+<<<<<<< HEAD
   turn=0
   while count <= board.length-1
     if board[count] =="O" || board[count] =="X"
       turn +=1
     end
     count += 1
+=======
+  turn=1
+  while count <= board.length-1
+    count += 1
+    if board[count] =="O" || board[count] =="X"
+      turn +=1
+    end
+>>>>>>> bba3b2a144b32f18e98d7bfb258883b7cff5292b
   end
   turn
 end
 
 def current_player(board)
+<<<<<<< HEAD
   current_player="O"
   if turn_count(board)%2==0
     current_player="X"
@@ -42,3 +52,10 @@ puts current_player(board5)
 #puts current_player(board3)
 #puts current_player(board4)
 #puts current_player(board5)
+=======
+  if turn_count%2==0
+    current_player="X"
+  elsif turn_count%2!=0
+    current_player="O"
+end
+>>>>>>> bba3b2a144b32f18e98d7bfb258883b7cff5292b
